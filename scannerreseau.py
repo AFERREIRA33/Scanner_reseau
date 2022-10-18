fc71786b2859dece3cb4f452f762a4411ccd7f58
AFERREIRA33 <alex.ferreira@ynov.com>
2022-10-18 11:01:58 +0200

    uuuuuuuuuuuuuuuuuuu

--- scannerreseau.py
+++ scannerreseau.py
@@ -2,20 +2,15 @@ import ipaddress
 import netifaces
 from netaddr import IPAddress
 import os
-from ipaddress import IPv4Network
 
-interfaces = netifaces.gateways()['default'][netifaces.AF_INET][1]
-interfaces = netifaces.ifaddresses(str(interfaces))
+interfaces = netifaces.ifaddresses(
+    str(netifaces.gateways()['default'][netifaces.AF_INET][1]))
 IpAddr = interfaces[netifaces.AF_INET][0]['addr']
 Netmask = interfaces[netifaces.AF_INET][0]['netmask']
-print('ip :' + IpAddr)
-
-print('netmask :'+Netmask)
 a = ipaddress.ip_network(
     IpAddr + '/'+str(IPAddress(Netmask).netmask_bits()), strict=False)
 print(a)
 print('----------------------------------')
-# a = IpAddr + '/' + ipaddress(Netmask).netmask_bits()
 for ip in ipaddress.IPv4Network(a):
     response = os.popen(f"ping {ip} -n 1").read()
     print(response)
