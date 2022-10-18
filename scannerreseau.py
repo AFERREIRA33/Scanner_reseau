e452c7e0f2d82b83b65fef1813f407cdde80055f
AFERREIRA33 <alex.ferreira@ynov.com>
2022-10-18 10:34:03 +0200

    ping all

--- scannerreseau.py
+++ scannerreseau.py
@@ -1,7 +1,21 @@
 import ipaddress
-import socket 
-hostname = socket.gethostname()    
-IPAddr = socket.gethostbyname(hostname)   
-print("Your Computer IP Address is:" + IPAddr) 
-# for ip in ipaddress.IPv4Network('192.168.1.0/24'):
-#     print(ip)
+import netifaces
+from netaddr import IPAddress
+import os
+from ipaddress import IPv4Network
+
+interfaces = netifaces.gateways()['default'][netifaces.AF_INET][1]
+interfaces = netifaces.ifaddresses(str(interfaces))
+IpAddr = interfaces[netifaces.AF_INET][0]['addr']
+Netmask = interfaces[netifaces.AF_INET][0]['netmask']
+print('ip :' + IpAddr)
+
+print('netmask :'+Netmask)
+a = ipaddress.ip_network(
+    IpAddr + '/'+str(IPAddress(Netmask).netmask_bits()), strict=False)
+print(a)
+print('----------------------------------')
+# a = IpAddr + '/' + ipaddress(Netmask).netmask_bits()
+for ip in ipaddress.IPv4Network(a):
+    response = os.popen(f"ping {ip} -n 1").read()
+    print(response)
