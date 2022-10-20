7c38e794f3cdd48b59966e09f95c7fe28d119050
JSELVA <joseph.selva@ynov.com>
2022-10-20 20:18:29 +0200

    python

--- scannerreseau.py
+++ scannerreseau.py
@@ -57,7 +57,7 @@ if len(sys.argv) < 1:
         ARPrequest()
     elif resarg == "Port":
         Portrequest()
-elif len(sys.argv) > 1:
+elif len(sys.argv) >=2 :
     print("too many argument"+"\n")
     print("-a : make arp request to all network"+"\n"+"-t : make tcp request to all network"+"\n"+"-p : scan port of choose ip"+"\n"+"-h : show command")
 else :
