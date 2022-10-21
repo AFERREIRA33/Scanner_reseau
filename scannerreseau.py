6f885d56c9c8b06c3357fa3361e2831e6bce3e61
AFERREIRA33 <alex.ferreira@ynov.com>
2022-10-21 08:51:00 +0200

    py

--- scannerreseau.py
+++ scannerreseau.py
@@ -13,8 +13,7 @@ print(i)
 interfaces = str(input())
 IpAddr = netifaces.ifaddresses(interfaces)[netifaces.AF_INET][0]['addr']
 Netmask = netifaces.ifaddresses(interfaces)[netifaces.AF_INET][0]['netmask']
-a = ipaddress.ip_network(
-    IpAddr + '/'+str(IPAddress(Netmask).netmask_bits()), strict=False)
+a = ipaddress.ip_network(IpAddr + '/'+str(IPAddress(Netmask).netmask_bits()), strict=False)
 print(a)
 ip = str(a)
 print('----------------------------------')
