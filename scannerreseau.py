27ff2ae6f9e80f269cc5668e4fc4c4b3d11ce7e7
JSELVA <joseph.selva@ynov.com>
2022-10-21 10:05:04 +0200

    os research

--- scannerreseau.py
+++ scannerreseau.py
@@ -1,6 +1,7 @@
 import ipaddress
 import netifaces
 from netaddr import IPAddress
+import re
 from scapy.all import *
 import sys
 from scapy.layers.inet import IP, ICMP
@@ -20,26 +21,31 @@ def ARPrequest():
     ans.summary()
 
 
-def TCPrequest():
+def osrequest():
     os = ''
     target = str(input("Enter the Ip address:"))
-    pack = IP(dst=target)/ICMP()
-    resp = sr1(pack, timeout=3)
-    if resp:
-        if IP in resp:
-            ttl = resp.getlayer(IP).ttl
-            print(ttl)
-            if ttl == 64: 
-                os = 'Linux'
-                print(f'\n\nTTL = {ttl} \n*{os}* Operating System is Detected \n\n')
-            elif ttl == 128:
-                os = 'Windows'
-                print(f'\n\nTTL = {ttl} \n*{os}* Operating System is Detected \n\n')
-            else:
-                print('Not Found')
+    reg = r"[^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$]"
+    if re.match(reg, target):
+        pack = IP(dst=target)/ICMP()
+        resp = sr1(pack, timeout=3)
+        if resp:
+            if IP in resp:
+                ttl = resp.getlayer(IP).ttl
+                print(ttl)
+                if ttl == 64: 
+                    os = 'Linux'
+                    print(f'\n\nTTL = {ttl} \n*{os}* Operating System is Detected \n\n')
+                elif ttl == 128:
+                    os = 'Windows'
+                    print(f'\n\nTTL = {ttl} \n*{os}* Operating System is Detected \n\n')
+                elif ttl == 255:
+                    os = 'Cisco Routeur'
+                    print(f'\n\nTTL = {ttl} \n*{os}* Operating System is Detected \n\n')
+                else:
+                    print('Not Found')
+    else :
+        print("please enter a valid ip")
             
-
-
 
 def Portrequest():
     print("work")
@@ -55,17 +61,17 @@ def Portrequest():
 def argumentstart(args):
     if args == "-h":
         print("-a : make arp request to all network"+"\n" +
-              "-t : make tcp request to all network"+"\n"+"-p : scan port of choose ip")
+              "-o : find the os of the choose ip"+"\n"+"-p : scan port of choose ip")
         return "help"
-    elif args == "-t":
-        return "TCP"
+    elif args == "-o":
+        return "os"
     elif args == "-p":
         return "Port"
     elif args == "-a":
         return "ARP"
     else:
         print("please enter a valid argument"+"\n")
-        print("-a : make arp request to all network"+"\n"+"-t : make tcp request to all network" +
+        print("-a : make arp request to all network"+"\n"+"-o : find the os of the choose ip" +
               "\n"+"-p : scan port of choose ip"+"\n"+"-h : show command")
         return "invalidargs"
 
@@ -73,51 +79,18 @@ def argumentstart(args):
 if len(sys.argv) <= 2 or len(sys.argv) > 1:
     args = str(sys.argv[1])
     resarg = argumentstart(args)
-    if resarg == "TCP":
-        TCPrequest()
+    if resarg == "os":
+        osrequest()
     elif resarg == "ARP":
         ARPrequest()
     elif resarg == "Port":
         Portrequest()
 elif len(sys.argv) >= 3:
     print("too many argument"+"\n")
-    print("-a : make arp request to all network"+"\n"+"-t : make tcp request to all network" +
+    print("-a : make arp request to all network"+"\n"+"-o : find the os of the choose ip" +
           "\n"+"-p : scan port of choose ip"+"\n"+"-h : show command")
 else:
     print("please enter a argument"+"\n")
-    print("-a : make arp request to all network"+"\n"+"-t : make tcp request to all network" +
+    print("-a : make arp request to all network"+"\n"+"-o : find the os of the choose ip" +
           "\n"+"-p : scan port of choose ip"+"\n"+"-h : show command")
 
-
-# PING_TIMEOUT = 1
-
-# i = netifaces.interfaces()
-# print(i)
-# interfaces = str(input())
-# IpAddr = netifaces.ifaddresses(interfaces)[netifaces.AF_INET][0]['addr']
-# Netmask = netifaces.ifaddresses(interfaces)[netifaces.AF_INET][0]['netmask']
-# a = ipaddress.ip_network(
-#     IpAddr + '/'+str(IPAddress(Netmask).netmask_bits()), strict=False)
-# print(a)
-# ip = str(a)
-# print('----------------------------------')
-# #for iparp in ans:
-# #    print(iparp)
-
-# #ans.summary( lambda s,r : r.sprintf("%IP.src% is alive") )
-# #ans.summary()
-
-
-# print(a)
-# ip = str(a)
-# print('----------------------------------')
-# ans, unans = arping(ip)
-# print(type(ans))
-# print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-# print(str(ans))
-# u = re.findall(r'[0-9]+(?:\.[0-9]+){3}', ans.summary())
-# print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-# print(u)
-# # ans, unans = sr(IP(dst=ip)/TCP(dport=53), loop=1)
-# a = ans.summary()
-# print(a)
